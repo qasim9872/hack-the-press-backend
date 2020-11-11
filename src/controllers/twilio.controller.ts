4286668e5cfb1f469c34a4652ac7d4443a088d96
Muhammad Qasim <qasim9872@gmail.com>
2020-11-11 20:45:33 +0000

    enabled twiml application

--- src/controllers/twilio.controller.ts
+++ src/controllers/twilio.controller.ts
@@ -4,10 +4,16 @@ import twilio from "twilio"
 const VoiceResponse = twilio.twiml.VoiceResponse
 export const client = twilio(ACCOUNT_SID, AUTH_TOKEN)
 
-export function say(text: string) {
+export function sayText(text: string) {
     const twiml = new VoiceResponse()
 
-    twiml.say(text)
+    twiml.say(
+        {
+            voice: "Polly.Amy",
+            language: "en-GB",
+        },
+        text
+    )
 
     return twiml.toString()
 }
