6f2d0e40282e8bb1b92d890753569a0826c3bd06
Muhammad Qasim <qasim9872@gmail.com>
2020-11-15 22:51:47 +0000

    refactored and disabled slack transport

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,13 +1,15 @@
 import pino from "pino"
 import { LOG_LEVEL, NAME, IS_PROD } from "@config/app.config"
 
-export const logger = pino({
+export const options = {
   name: NAME,
   level: LOG_LEVEL,
   prettyPrint: !IS_PROD,
   customLevels: {
     notifyOnSlack: 35,
   },
-})
+}
+
+export const logger = pino(options)
 
 export default logger
