e99e1de83a3771ef0b0072ef8b8e3ecbcc37d159
Muhammad Qasim <qasim9872@gmail.com>
2022-02-12 14:09:58 +0000

    more cleanup

--- src/models/my-bot.model.ts
+++ src/models/my-bot.model.ts
@@ -8,7 +8,6 @@ import {
   setGlobalOptions,
   Severity,
 } from "@typegoose/typegoose"
-import { IntentAnswerPair, TwimlConfig } from "./intent-answer-pair"
 import Boom from "boom"
 
 // This is to allow nested objects
@@ -18,7 +17,6 @@ export interface MyBot {
   name: string
   details: string
   phoneNumbers: string[]
-  intentAnswerPairs: IntentAnswerPair[]
 }
 
 @plugin(timestamps)
@@ -44,61 +42,8 @@ export class MyBot implements MyBot {
   })
   public phoneNumbers: string[] = []
 
-  @prop({ required: true })
-  public intentAnswerPairs: IntentAnswerPair[] = []
-
   public static async findByPhoneNumber(this: ReturnModelType<typeof MyBot>, phoneNumber: string) {
     return this.findOne({ phoneNumbers: phoneNumber }).exec()
-  }
-
-  public findIntent(this: DocumentType<MyBot>, intent: string) {
-    return this.intentAnswerPairs.find((intentAnswerPair: IntentAnswerPair) => intentAnswerPair.intent === intent)
-  }
-
-  public async addIntent(this: DocumentType<MyBot>, intent: string) {
-    if (this.findIntent(intent)) {
-      throw new Boom(`intent: ${intent} already exists`)
-    }
-
-    const intentAnswerPair: IntentAnswerPair = {
-      intent: intent,
-      response: [],
-    }
-
-    this.intentAnswerPairs.push(intentAnswerPair)
-    return this.save()
-  }
-
-  public async addResponse(
-    this: DocumentType<MyBot>,
-    intent: string,
-    response: string[],
-    config?: TwimlConfig,
-    create = true,
-    set = false
-  ) {
-    if (!this.findIntent(intent) && create) {
-      await this.addIntent(intent)
-    }
-
-    const intentAnswerPair = this.findIntent(intent)
-    if (!intentAnswerPair) {
-      throw new Boom(`intentAnswerPair not found for ${intent}`)
-    }
-
-    if (set) {
-      intentAnswerPair.response = response
-    } else {
-      intentAnswerPair.response.push(...response)
-    }
-
-    if (config) {
-      intentAnswerPair.config = config
-    }
-
-    this.markModified("intentAnswerPairs")
-
-    return this.save()
   }
 }
 
